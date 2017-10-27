d2bc377c315dc48e7aed148f9d13a11bc37896a2
Todd Miller <tmiller@fairwaytech.com>
2017-10-27 09:08:45 -0700

    Replace tabs with spaces

--- service/src/main/java/tds/apl/TdsAssessmentPackageLoaderApplication.java
+++ service/src/main/java/tds/apl/TdsAssessmentPackageLoaderApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TdsAssessmentPackageLoaderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TdsAssessmentPackageLoaderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TdsAssessmentPackageLoaderApplication.class, args);
+    }
 }
