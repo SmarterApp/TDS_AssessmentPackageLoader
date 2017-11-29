d11e2f5c5888a8751a2864a74e9581aa549c471d
Ernie Munoz <emunoz@fairwaytech.com>
2017-11-29 13:03:43 -0800

    Reverting accidentally committed controller changes

--- service/src/main/java/tds/support/tool/web/TestPackageController.java
+++ service/src/main/java/tds/support/tool/web/TestPackageController.java
@@ -3,7 +3,6 @@ package tds.support.tool.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 import tds.support.job.Job;
 import tds.support.tool.services.JobService;
@@ -37,10 +35,5 @@ public class TestPackageController {
     public ResponseEntity<Job> loadPackage(@RequestParam("file") MultipartFile file) throws IOException {
         Job job = jobService.startPackageImport(file.getOriginalFilename(), file.getInputStream(), file.getSize());
         return ResponseEntity.ok(job);
-    }
-
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Job>> getJobs(@RequestParam(value="withinHours", required = false) Integer withinHours) {
-
     }
 }
