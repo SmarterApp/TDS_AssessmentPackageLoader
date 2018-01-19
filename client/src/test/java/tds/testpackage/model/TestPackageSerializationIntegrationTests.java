caac52b464b12594fd924aeab48a83947da618fb
Mike Trupkin <mtrupkin@fairwaytech.com>
2018-01-19 10:06:06 -0800

    convert indentation from tabs to spaces

--- client/src/test/java/tds/testpackage/model/TestPackageSerializationIntegrationTests.java
+++ client/src/test/java/tds/testpackage/model/TestPackageSerializationIntegrationTests.java
@@ -24,8 +24,8 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 public class TestPackageSerializationIntegrationTests {
-	private ObjectMapper objectMapper;
-	private XmlMapper xmlMapper;
+    private ObjectMapper objectMapper;
+    private XmlMapper xmlMapper;
 
     private String expectedJSON = "{\"publisher\":\"SBAC_PT\",\"publishDate\":\"2015-08-19T18:13:51.0\",\"subject\":\"MATH\",\"type\":\"summative\",\"version\":\"8185\",\"bankKey\":187,\"academicYear\":\"2017-2018\"," +
         "\"blueprint\":[{\"id\":\"SBAC-IRP-COMBINED-MATH-11\",\"type\":\"combined\"}]," +
@@ -38,147 +38,147 @@ public class TestPackageSerializationIntegrationTests {
         "\"position\":1}],\"tools\":[{\"name\":\"tool\",\"studentPackageFieldName\":\"TDSAcc\"," +
         "\"options\":[{\"code\":\"TDS_Other\",\"sortOrder\":0,\"dependencies\":[{\"ifToolType\":\"ifToolType\",\"ifToolCode\":\"ifToolCode\",\"enabled\":true,\"default\":false}]}]}]}]}";
 
-	@Before
-	public void setUp() {
-		objectMapper = new ObjectMapper();
-		objectMapper.registerModule(new Jdk8Module());
-		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+    @Before
+    public void setUp() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
 
-		xmlMapper = new XmlMapper();
-		xmlMapper.registerModule(new Jdk8Module());
-	}
+        xmlMapper = new XmlMapper();
+        xmlMapper.registerModule(new Jdk8Module());
+    }
 
-	@Test
-	public void shouldSerializeToJson() throws JsonProcessingException {
-		BlueprintElement blueprintElement = BlueprintElement.builder()
-			.setId("SBAC-IRP-COMBINED-MATH-11")
-			.setType("combined")
-			.build();
+    @Test
+    public void shouldSerializeToJson() throws JsonProcessingException {
+        BlueprintElement blueprintElement = BlueprintElement.builder()
+            .setId("SBAC-IRP-COMBINED-MATH-11")
+            .setType("combined")
+            .build();
 
-		Grade grade = Grade.builder()
-			.setValue("11")
-			.build();
+        Grade grade = Grade.builder()
+            .setValue("11")
+            .build();
 
-		BlueprintReference blueprintReference1 = BlueprintReference.builder()
-			.setIdRef("SBAC-IRP-CAT-MATH-11")
-			.build();
+        BlueprintReference blueprintReference1 = BlueprintReference.builder()
+            .setIdRef("SBAC-IRP-CAT-MATH-11")
+            .build();
 
         BlueprintReference blueprintReference2 = BlueprintReference.builder()
             .setIdRef("G11Math_DOK2")
             .build();
 
-		ItemScoreParameter itemScoreParameter = ItemScoreParameter.builder()
-			.setMeasurementParameter("a")
-			.setValue(6.3)
-			.build();
+        ItemScoreParameter itemScoreParameter = ItemScoreParameter.builder()
+            .setMeasurementParameter("a")
+            .setValue(6.3)
+            .build();
 
-		ItemScoreDimension itemScoreDimension = ItemScoreDimension.builder()
-			.setScorePoints(1)
-			.setMeasurementModel("IRT3PLn")
-			.setWeight(1)
-			.setItemScoreParameters(Arrays.asList(itemScoreParameter))
-			.build();
+        ItemScoreDimension itemScoreDimension = ItemScoreDimension.builder()
+            .setScorePoints(1)
+            .setMeasurementModel("IRT3PLn")
+            .setWeight(1)
+            .setItemScoreParameters(Arrays.asList(itemScoreParameter))
+            .build();
 
-		Item item = Item.builder()
-			.setPresentations(Arrays.asList("ENU"))
-			.setBlueprintReferences(Arrays.asList(blueprintReference1, blueprintReference2))
-			.setId("id")
-			.setType("type")
-			.setItemScoreDimension(itemScoreDimension)
-			.build();
+        Item item = Item.builder()
+            .setPresentations(Arrays.asList("ENU"))
+            .setBlueprintReferences(Arrays.asList(blueprintReference1, blueprintReference2))
+            .setId("id")
+            .setType("type")
+            .setItemScoreDimension(itemScoreDimension)
+            .build();
 
-		ItemGroup itemGroup = ItemGroup.builder()
-			.setId("id")
-			.setItems(Arrays.asList(item))
-			.build();
+        ItemGroup itemGroup = ItemGroup.builder()
+            .setId("id")
+            .setItems(Arrays.asList(item))
+            .build();
 
-		Segment segment = Segment.builder()
-			.setId("SBAC-IRP-Perf-MATH-11")
-			.setPosition(Optional.of(1))
-			.setAlgorithmType("fixedform")
-			.setAlgorithmImplementation("FAIRWAY ROUNDROBIN")
-			.setPool(Arrays.asList(itemGroup))
-			.build();
+        Segment segment = Segment.builder()
+            .setId("SBAC-IRP-Perf-MATH-11")
+            .setPosition(Optional.of(1))
+            .setAlgorithmType("fixedform")
+            .setAlgorithmImplementation("FAIRWAY ROUNDROBIN")
+            .setPool(Arrays.asList(itemGroup))
+            .build();
 
-		Dependency dependency = Dependency.builder()
-			.setIfToolType("ifToolType")
-			.setIfToolCode("ifToolCode")
-			.build();
+        Dependency dependency = Dependency.builder()
+            .setIfToolType("ifToolType")
+            .setIfToolCode("ifToolCode")
+            .build();
 
-		Option option = Option.builder()
-			.setCode("TDS_Other")
-			.setSortOrder(0)
-			.setDependencies(Arrays.asList(dependency))
-			.build();
+        Option option = Option.builder()
+            .setCode("TDS_Other")
+            .setSortOrder(0)
+            .setDependencies(Arrays.asList(dependency))
+            .build();
 
-		Tool tool = Tool.builder()
-			.setName("tool")
-			.setStudentPackageFieldName("TDSAcc")
-			.setOptions(Arrays.asList(option))
-			.build();
+        Tool tool = Tool.builder()
+            .setName("tool")
+            .setStudentPackageFieldName("TDSAcc")
+            .setOptions(Arrays.asList(option))
+            .build();
 
-		Assessment assessment = Assessment.builder()
-			.setId("SBAC-IRP-CAT-MATH-11")
-			.setLabel("IRP CAT Grade 11 Math")
-			.setGrades(Arrays.asList(grade))
-			.setSegments(Arrays.asList(segment))
-			.setTools(Arrays.asList(tool))
-			.build();
+        Assessment assessment = Assessment.builder()
+            .setId("SBAC-IRP-CAT-MATH-11")
+            .setLabel("IRP CAT Grade 11 Math")
+            .setGrades(Arrays.asList(grade))
+            .setSegments(Arrays.asList(segment))
+            .setTools(Arrays.asList(tool))
+            .build();
 
-		TestPackage testPackage = TestPackage.builder()
-			.setPublisher("SBAC_PT")
-			.setPublishDate("2015-08-19T18:13:51.0")
-			.setSubject("MATH")
-			.setType("summative")
-			.setVersion("8185")
-			.setBankKey(187)
-			.setAcademicYear("2017-2018")
-			.setBlueprint(Arrays.asList(blueprintElement))
-			.setAssessments(Arrays.asList(assessment))
-			.build();
+        TestPackage testPackage = TestPackage.builder()
+            .setPublisher("SBAC_PT")
+            .setPublishDate("2015-08-19T18:13:51.0")
+            .setSubject("MATH")
+            .setType("summative")
+            .setVersion("8185")
+            .setBankKey(187)
+            .setAcademicYear("2017-2018")
+            .setBlueprint(Arrays.asList(blueprintElement))
+            .setAssessments(Arrays.asList(assessment))
+            .build();
 
-		String json = objectMapper.writeValueAsString(testPackage);
+        String json = objectMapper.writeValueAsString(testPackage);
 
-		assertThatJson(json)
-			.isEqualTo(expectedJSON);
-	}
+        assertThatJson(json)
+            .isEqualTo(expectedJSON);
+    }
 
     @Test
-	public void shouldDeserializeFromXml() throws IOException {
-		InputStream inputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.xml");
-		TestPackage testPackage = xmlMapper.readValue(inputStream, TestPackage.class);
-		assertThat(testPackage.getPublisher()).isEqualTo("SBAC_PT");
-		assertThat(testPackage.getSubject()).isEqualTo("MATH");
+    public void shouldDeserializeFromXml() throws IOException {
+        InputStream inputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.xml");
+        TestPackage testPackage = xmlMapper.readValue(inputStream, TestPackage.class);
+        assertThat(testPackage.getPublisher()).isEqualTo("SBAC_PT");
+        assertThat(testPackage.getSubject()).isEqualTo("MATH");
         assertThat(testPackage.getAssessments().get(0).getSegments().get(0).getPool().get(0).getItems().get(0).getBlueprintReferences().size()).isEqualTo(3);
-	}
+    }
 
-	@Test
-	public void shouldDeserializeItemFromXmlWithAssessmentKey() throws IOException {
+    @Test
+    public void shouldDeserializeItemFromXmlWithAssessmentKey() throws IOException {
         JacksonXmlModule module = new JacksonXmlModule();
-		module.addDeserializer(TestPackage.class, new TestPackageDeserializer());
-		xmlMapper.registerModule(module);
+        module.addDeserializer(TestPackage.class, new TestPackageDeserializer());
+        xmlMapper.registerModule(module);
 
-		InputStream inputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.xml");
-		TestPackage testPackage = xmlMapper.readValue(inputStream, TestPackage.class);
-		assertThat(testPackage.getPublisher()).isEqualTo("SBAC_PT");
-		assertThat(testPackage.getSubject()).isEqualTo("MATH");
-		assertThat(testPackage.getAssessments().get(0).getSegments().get(0).getPool().get(0).getItems().get(0).getBlueprintReferences().size()).isEqualTo(3);
-		assertThat(testPackage.getAssessments().get(0).getKey()).isEqualTo("(SBAC_PT)SBAC-IRP-CAT-MATH-11-2017-2018");
-		assertThat(testPackage.getAssessments().get(0).getSegments().get(0).getKey()).isEqualTo("(SBAC_PT)SBAC-IRP-CAT-MATH-11-2017-2018");
-	}
+        InputStream inputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.xml");
+        TestPackage testPackage = xmlMapper.readValue(inputStream, TestPackage.class);
+        assertThat(testPackage.getPublisher()).isEqualTo("SBAC_PT");
+        assertThat(testPackage.getSubject()).isEqualTo("MATH");
+        assertThat(testPackage.getAssessments().get(0).getSegments().get(0).getPool().get(0).getItems().get(0).getBlueprintReferences().size()).isEqualTo(3);
+        assertThat(testPackage.getAssessments().get(0).getKey()).isEqualTo("(SBAC_PT)SBAC-IRP-CAT-MATH-11-2017-2018");
+        assertThat(testPackage.getAssessments().get(0).getSegments().get(0).getKey()).isEqualTo("(SBAC_PT)SBAC-IRP-CAT-MATH-11-2017-2018");
+    }
 
-	/**
-	 * Ensure that optional values are not deserialized as null.
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void shouldDeserializeWithoutNulls() throws IOException {
-		InputStream inputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("scoring-rules.xml");
-		TestPackage testPackage = xmlMapper.readValue(inputStream, TestPackage.class);
-		String json = objectMapper.writeValueAsString(testPackage);
-		assertThat(json.indexOf("null")).isEqualTo(-1);
-	}
+    /**
+     * Ensure that optional values are not deserialized as null.
+     *
+     * @throws IOException
+     */
+    @Test
+    public void shouldDeserializeWithoutNulls() throws IOException {
+        InputStream inputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("scoring-rules.xml");
+        TestPackage testPackage = xmlMapper.readValue(inputStream, TestPackage.class);
+        String json = objectMapper.writeValueAsString(testPackage);
+        assertThat(json.indexOf("null")).isEqualTo(-1);
+    }
 
     @Test
     public void shouldDeserializePresentationsWithoutTrailingWhitespace() throws IOException {
@@ -188,47 +188,48 @@ public class TestPackageSerializationIntegrationTests {
         assertThat(presentation).isEqualTo(presentation.trim());
     }
 
-	@Test
-	public void shouldDeserializeFromXmlWithBooleanDefaults() throws IOException {
-		InputStream inputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("Dependencies-boolean-defaults.xml");
-		List<Dependency> dependencies = xmlMapper.readValue(inputStream, new TypeReference<List<Dependency>>() {});
+    @Test
+    public void shouldDeserializeFromXmlWithBooleanDefaults() throws IOException {
+        InputStream inputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("Dependencies-boolean-defaults.xml");
+        List<Dependency> dependencies = xmlMapper.readValue(inputStream, new TypeReference<List<Dependency>>() {
+        });
 
-		assertThat(dependencies.size()).isEqualTo(7);
-		assertThat(dependencies.get(0).enabled()).isEqualTo(false);
-		assertThat(dependencies.get(1).enabled()).isEqualTo(true);
-		assertThat(dependencies.get(3).enabled()).isEqualTo(false);
-		assertThat(dependencies.get(4).enabled()).isEqualTo(true);
+        assertThat(dependencies.size()).isEqualTo(7);
+        assertThat(dependencies.get(0).enabled()).isEqualTo(false);
+        assertThat(dependencies.get(1).enabled()).isEqualTo(true);
+        assertThat(dependencies.get(3).enabled()).isEqualTo(false);
+        assertThat(dependencies.get(4).enabled()).isEqualTo(true);
 
-		assertThat(dependencies.get(0).defaultValue()).isEqualTo(false);
-		assertThat(dependencies.get(2).defaultValue()).isEqualTo(true);
-		assertThat(dependencies.get(5).defaultValue()).isEqualTo(true);
-		assertThat(dependencies.get(6).defaultValue()).isEqualTo(false);
-	}
+        assertThat(dependencies.get(0).defaultValue()).isEqualTo(false);
+        assertThat(dependencies.get(2).defaultValue()).isEqualTo(true);
+        assertThat(dependencies.get(5).defaultValue()).isEqualTo(true);
+        assertThat(dependencies.get(6).defaultValue()).isEqualTo(false);
+    }
 
-	@Test
-	public void shouldDeserializeFromJson() throws IOException {
-		TestPackage testPackage = objectMapper.readValue(expectedJSON, TestPackage.class);
+    @Test
+    public void shouldDeserializeFromJson() throws IOException {
+        TestPackage testPackage = objectMapper.readValue(expectedJSON, TestPackage.class);
 
-		assertThat(testPackage.getPublisher()).isEqualTo("SBAC_PT");
-		assertThat(testPackage.getSubject()).isEqualTo("MATH");
-	}
+        assertThat(testPackage.getPublisher()).isEqualTo("SBAC_PT");
+        assertThat(testPackage.getSubject()).isEqualTo("MATH");
+    }
 
-	/**
-	 * Both the files below represent the same test package:
-	 * V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.xml and V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.json
-	 *
-	 * Deserializing xml and json formats and comparing the in memory object model.
-	 *
-	 * @throws IOException
-	 */
-	@Test
-	public void shouldDeserializeFromJsonAndXmlAsSameObject() throws IOException {
-		InputStream xmlInputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.xml");
-		InputStream jsonInputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.json");
+    /**
+     * Both the files below represent the same test package:
+     * V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.xml and V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.json
+     * <p>
+     * Deserializing xml and json formats and comparing the in memory object model.
+     *
+     * @throws IOException
+     */
+    @Test
+    public void shouldDeserializeFromJsonAndXmlAsSameObject() throws IOException {
+        InputStream xmlInputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.xml");
+        InputStream jsonInputStream = TestPackageSerializationIntegrationTests.class.getClassLoader().getResourceAsStream("V2-(SBAC_PT)IRP-GRADE-11-MATH-EXAMPLE.json");
 
-		TestPackage testPackageXml = xmlMapper.readValue(xmlInputStream, TestPackage.class);
-		TestPackage testPackageJson = objectMapper.readValue(jsonInputStream, TestPackage.class);
+        TestPackage testPackageXml = xmlMapper.readValue(xmlInputStream, TestPackage.class);
+        TestPackage testPackageJson = objectMapper.readValue(jsonInputStream, TestPackage.class);
 
-		assertThat(testPackageXml).isEqualTo(testPackageJson);
-	}
+        assertThat(testPackageXml).isEqualTo(testPackageJson);
+    }
 }
