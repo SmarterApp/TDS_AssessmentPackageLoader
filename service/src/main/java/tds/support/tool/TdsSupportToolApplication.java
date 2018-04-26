06f9cca56b2ad2b21b787fa37832b4c9c39df11d
Mike Trupkin <mtrupkin@fairwaytech.com>
2018-04-26 10:43:22 -0700

    Since SAML was enabled, standard resource forwarding was not functioning as excepted. Loading the resource from the class path and sending it in the response.

--- service/src/main/java/tds/support/tool/TdsSupportToolApplication.java
+++ service/src/main/java/tds/support/tool/TdsSupportToolApplication.java
@@ -1,16 +1,29 @@
 package tds.support.tool;
 
+import com.google.common.io.ByteStreams;
+import org.apache.http.entity.ContentType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class TdsSupportToolApplication {
+
+    // Map all urls that do not have an extension (ie. image.png)
+    // to the angular app entry point (index.html)
+    //
+    // With SAML enabled, using the standard spring forwarding mechanism was not functioning.
     @GetMapping(value = "/**/{path:[^\\.]*}")
-    public String redirect() {
-        return "forward:/";
+    public void redirect(HttpServletResponse response) throws Exception {
+        response.setContentType(ContentType.TEXT_HTML.getMimeType());
+        final InputStream inputStream = new ClassPathResource("/static/index.html").getInputStream();
+        ByteStreams.copy(inputStream, response.getOutputStream());
     }
 
     public static void main(String[] args) {
