0aa567ca5addc9aa58dac598f43fd5db8fffae32
Mike Trupkin <mtrupkin@fairwaytech.com>
2018-06-12 17:11:31 -0700

    fix loading rubric list from content service
    Content service was unmarshalling rubric list from object model found in item-renderer project and could not be loaded by
    the teacher handscoring application.

--- service/src/test/java/tds/support/tool/services/JobServiceTest.java
+++ service/src/test/java/tds/support/tool/services/JobServiceTest.java
@@ -11,11 +11,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.*;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
@@ -36,6 +39,7 @@ import tds.support.tool.handlers.loader.TestPackageHandler;
 import tds.support.tool.repositories.JobRepository;
 import tds.support.tool.repositories.loader.TestPackageRepository;
 import tds.support.tool.services.impl.JobServiceImpl;
+import tds.support.tool.services.impl.SupportToolTestConfiguration;
 import tds.support.tool.services.impl.THSSServiceImpl;
 import tds.support.tool.services.loader.MessagingService;
 import tds.support.tool.testpackage.configuration.TestPackageObjectMapperConfiguration;
@@ -47,128 +51,37 @@ import static org.springframework.test.web.client.ExpectedCount.manyTimes;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-@RunWith(SpringRunner.class)
-@ComponentScan(basePackages = {
-    "tds.support.tool.testpackage.configuration",
-    "tds.support.tool.handlers.loader",
-    "tds.support.tool.services.loader",
-    "tds.support.tool.handlers.loader.impl",
-    "tds.support.tool.services.impl",
-    "tds.support.tool.validation"})
-@Import({SupportToolServiceConfiguration.class, SupportToolProperties.class})
-@DataMongoTest
+
 /**
  * Runs the support tool load package job.
  * Mocks external services: (MongoDB, Microservices, THSS)
  *
  * Excludes spring from auto configuring RabbitMQ.
  */
+@RunWith(SpringRunner.class)
+@DataMongoTest
 public class JobServiceTest {
     /**
      * THSS Service uses Apache HTTP Client, which is not as
      * convenient to mock as the Spring Rest Template.
      */
+    @TestConfiguration
+    @Import({SupportToolTestConfiguration.class})
     public static class JobServiceConfiguration {
-        @Autowired
-        SupportToolProperties supportToolProperties;
-        @Autowired
-        TestPackageObjectMapperConfiguration testPackageObjectMapperConfiguration;
-        @Autowired
-        RestTemplate integrationRestTemplate;
-
-        @Bean
-        @Primary
-        public THSSService mockService()
-        {
-            // Returns an Http Client that conditionally returns different json responses
-            // depending on the contents of the http request body.
-
-            Supplier<CloseableHttpClient> httpClientSupplier = () -> {
-                HttpClientMock httpClientMock = new HttpClientMock();
-                httpClientMock.onPost("http://localhost:28039/api/item/submit").
-                    withBody(containsString("success")).
-                    doReturnJSON("{\n" +
-                    "    \"Files\": [\n" +
-                    "        {\n" +
-                    "            \"FileName\": \"thss-example.json\",\n" +
-                    "            \"Success\": true,\n" +
-                    "            \"ErrorMessage\": null\n" +
-                    "        }\n" +
-                    "    ]\n" +
-                    "}");
-
-                httpClientMock.onPost("http://localhost:28039/api/item/submit").
-                    withBody(containsString("error")).
-                    doReturnJSON("{\n" +
-                    "    \"Files\": [\n" +
-                    "        {\n" +
-                    "            \"FileName\": \"thss-example.json\",\n" +
-                    "            \"Success\": false,\n" +
-                    "            \"ErrorMessage\": \"Mock Error Message\"\n" +
-                    "        }\n" +
-                    "    ]\n" +
-                    "}");
-
-                return httpClientMock;
-            };
-
-            return new THSSServiceImpl("/tds/bank/items/Item-%1$s-%2$s/item-%1$s-%2$s.xml", httpClientSupplier, supportToolProperties, integrationRestTemplate, testPackageObjectMapperConfiguration);
-        }
     }
 
     @Autowired
-    TestPackageObjectMapperConfiguration testPackageObjectMapperConfiguration;
-    @Autowired
     private JobRepository jobRepository;
     @Autowired
-    private TestPackageFileHandler testPackageFileHandler;
-    @Qualifier("testPackageLoaderStepHandlers")
+    private JobService jobService;
     @Autowired
-    private Map<String, TestPackageHandler> testPackageLoaderStepHandlers;
-    @Autowired
-    private TestPackageStatusService testPackageStatusService;
-    @Autowired
-    private RestTemplate integrationRestTemplate;
+    private SupportToolTestConfiguration supportToolTestConfiguration;
 
-    @MockBean
-    AmazonS3 amazonS3;
-    @MockBean
-    TestPackageRepository testPackageRepository;
-    @MockBean
-    MessagingService messagingService;
-
-    JobService jobService;
-    MockRestServiceServer mockServer;
-
-    static String RUBRIC_LIST =
-        "<rubriclist>\n" +
-        "   <rubric scorepoint=\"4\">\n" +
-        "       <name>        Rubric 4</name>\n" +
-        "       <val>\n" +
-        "           <![CDATA[<p style=\"\">&#xA0;</p><p><dl><dt style=\"float: left; margin-top: 1px; margin-bottom: -1em;\">a)</dt><dd style=\"float: top;padding-left:1.5000em; padding-left:1.5000em; \"><p style=\"font-style:normal; font-weight:normal; \">Inference here.</p></dd><dt style=\"float: left; margin-top: 1px; margin-bottom: -1em;\">b)</dt><dd style=\"float: top;padding-left:1.5000em; padding-left:1.5000em; \"><p style=\"font-style:normal; font-weight:normal; \">Text-supported example: “....” (paragraph reference)</p></dd><dt style=\"float: left; margin-top: 1px; margin-bottom: -1em;\">c)</dt><dd style=\"float: top;padding-left:1.5000em; padding-left:1.5000em; \"><p style=\"font-style:normal; font-weight:normal; \">Inference here.</p></dd><dt style=\"float: left; margin-top: 1px; margin-bottom: -1em;\">d)</dt><dd style=\"float: top;padding-left:1.5000em; padding-left:1.5000em; \"><p style=\"font-style:normal; font-weight:normal; \">Text-supported example: “....” (paragraph reference)</p></dd></dl></p>]]>\n" +
-        "       </val>\n" +
-        "   </rubric>\n" +
-        "   <samplelist maxval=\"4\" minval=\"4\">\n" +
-        "       <sample purpose=\"OtherExemplar\" scorepoint=\"4\">\n" +
-        "           <name>4-Point Other Official Sample Answers      </name>\n" +
-        "           <annotation />\n" +
-        "           <samplecontent>\n" +
-        "               <![CDATA[<p style=\"\">&#xA0;</p>]]>\n" +
-        "           </samplecontent>\n" +
-        "       </sample>\n" +
-        "   </samplelist>\n" +
-        "</rubriclist>";
-
+    private MockRestServiceServer mockServer;
 
     @Before
     public void setup() throws Exception {
-        integrationRestTemplate.setInterceptors(Arrays.asList());
-        ObjectMapper objectMapper = testPackageObjectMapperConfiguration.getThssObjectMapper();
-        jobService = new JobServiceImpl(jobRepository, testPackageFileHandler, messagingService, testPackageStatusService, testPackageLoaderStepHandlers);
-        mockServer = MockRestServiceServer.bindTo(integrationRestTemplate).ignoreExpectOrder(true).build();
-        String jsonResponse = objectMapper.writeValueAsString(Optional.of(RUBRIC_LIST));
-        mockServer.expect(manyTimes(), method(HttpMethod.GET)).andRespond(withSuccess(jsonResponse, MediaType.APPLICATION_JSON_UTF8));
-        mockServer.expect(manyTimes(), method(HttpMethod.POST)).andRespond(withSuccess());
+        mockServer = supportToolTestConfiguration.setUpMockServer();
     }
 
     @After
@@ -177,7 +90,7 @@ public class JobServiceTest {
     }
 
     @Test
-    @Ignore
+    @WithMockUser(username = "admin", authorities = { "PERM_SUPPORT_TOOL_ADMINISTRATION"})
     public void shouldLoadTestSpecificationPackage() {
         String filename = "thss-test-specification-success-example.xml";
         InputStream inputStream = JobServiceTest.class.getClassLoader().getResourceAsStream("thss-test-specification-example-1.xml");
@@ -192,7 +105,7 @@ public class JobServiceTest {
 
 
     @Test
-    @Ignore
+    @WithMockUser(username = "admin", authorities = { "PERM_SUPPORT_TOOL_ADMINISTRATION"})
     public void shouldRollbackWhenTHSSReturnsError() {
         String filename = "thss-test-specification-error-example.xml";
         InputStream inputStream = JobServiceTest.class.getClassLoader().getResourceAsStream("thss-test-specification-example-1.xml");
