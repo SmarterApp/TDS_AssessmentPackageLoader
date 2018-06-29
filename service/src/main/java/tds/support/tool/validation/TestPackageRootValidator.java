55c930a03a4bbe867cf4597054f488782810ae41
Jeffrey C Jones <jjones@fairwaytech.com>
2018-06-29 00:45:25 -0700

    TDS-1659: Reject unknown test package statuses. Statuses configured in app yml.

--- service/src/main/java/tds/support/tool/validation/TestPackageRootValidator.java
+++ service/src/main/java/tds/support/tool/validation/TestPackageRootValidator.java
@@ -1,19 +1,42 @@
 package tds.support.tool.validation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import tds.support.job.ErrorSeverity;
-import tds.testpackage.model.TestPackage;
 
 import javax.xml.bind.DatatypeConverter;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
+
+import tds.support.job.ErrorSeverity;
+import tds.support.tool.configuration.SupportToolProperties;
+import tds.testpackage.model.TestPackage;
 
 @Component
 public class TestPackageRootValidator implements TestPackageValidator {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestPackageRootValidator.class);
+    private final List<String> subjects;
+
+    @Autowired
+    public TestPackageRootValidator(final SupportToolProperties properties) {
+        if(properties.getSubjects() != null && properties.getSubjects().size() > 0) {
+            this.subjects = properties.getSubjects();
+        } else {
+            this.subjects = Collections.emptyList();
+            logger.error("No test package subjects defined in application.yml - all packages will fail validation!");
+        };
+    }
+
     @Override
     public void validate(final TestPackage testPackage, final List<ValidationError> errors) {
+        if (!subjects.contains(testPackage.getSubject())) {
+            errors.add(new ValidationError(ErrorSeverity.CRITICAL,
+                String.format("The test package subject '%s' must be one of: %s",
+                    testPackage.getSubject(), subjects)));
+        }
+
         validateTestPackageVersionIsLong(testPackage, errors);
 
         if (!isValidFormat(testPackage.getPublishDate())) {
--- service/src/test/java/tds/support/tool/validation/TestPackageRootValidatorTest.java
+++ service/src/test/java/tds/support/tool/validation/TestPackageRootValidatorTest.java
@@ -2,21 +2,34 @@ package tds.support.tool.validation;
 
 import org.junit.Before;
 import org.junit.Test;
-import tds.support.job.ErrorSeverity;
-import tds.testpackage.model.TestPackage;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import tds.support.job.ErrorSeverity;
+import tds.support.tool.configuration.SupportToolProperties;
+import tds.testpackage.model.TestPackage;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TestPackageRootValidatorTest extends TestPackageValidationBaseTest{
     private TestPackageRootValidator validator;
 
+    @Mock
+    private SupportToolProperties supportToolProperties;
+
     @Before
     public void setup() {
-        validator = new TestPackageRootValidator();
+        when(supportToolProperties.getSubjects()).thenReturn(
+            new ArrayList<String>(Arrays.asList(new String[] {"FOO", "BAZ", "MATH"})));
+        validator = new TestPackageRootValidator(supportToolProperties);
     }
 
     @Test
@@ -34,12 +47,20 @@ public class TestPackageRootValidatorTest extends TestPackageValidationBaseTest{
         List<ValidationError> errors = new ArrayList<>();
         validator.validate(invalidTestPackage, errors);
 
-        assertThat(errors).hasSize(2);
+        assertThat(errors).hasSize(3);
+
+        // TDS-1659 - some test suite files contained 'Mathematics' instead of 'MATH'.
         assertThat(errors.get(0).getSeverity()).isEqualTo(ErrorSeverity.CRITICAL);
-        assertThat(errors.get(0).getMessage()).isEqualTo("The test package version must be a long or integer value");
+        assertThat(errors.get(0).getMessage()).isEqualTo(
+            "The test package subject 'MATHEMATICS' must be one of: [FOO, BAZ, MATH]");
 
         assertThat(errors.get(1).getSeverity()).isEqualTo(ErrorSeverity.CRITICAL);
-        assertThat(errors.get(1).getMessage()).isEqualTo("The test package \"publishdate\" is not in a valid date format. Example format: 2015-08-19T22:44:00Z");
+        assertThat(errors.get(1).getMessage()).isEqualTo(
+            "The test package version must be a long or integer value");
+
+        assertThat(errors.get(2).getSeverity()).isEqualTo(ErrorSeverity.CRITICAL);
+        assertThat(errors.get(2).getMessage()).isEqualTo(
+            "The test package \"publishdate\" is not in a valid date format. Example format: 2015-08-19T22:44:00Z");
     }
 
 }
