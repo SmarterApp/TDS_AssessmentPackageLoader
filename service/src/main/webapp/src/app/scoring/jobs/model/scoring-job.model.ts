a5c41921b6554efcf12ec942822e8438926801bb
Ernie Muñoz <emunoz@fairwaytech.com>
2018-08-17 11:45:26 -0700

    Returning "----" string when properties are null for scoring jobs

--- service/src/main/webapp/src/app/scoring/jobs/model/scoring-job.model.ts
+++ service/src/main/webapp/src/app/scoring/jobs/model/scoring-job.model.ts
@@ -4,16 +4,52 @@ import {StepStatus} from "../../../testpackage/loader/jobs/model/test-package-jo
 export class ScoringJob {
   id: string;
   shortId: string;
-  examId: string;
-  assessmentId: string;
-  studentName: string;
   createdAt: Date;
   steps: ScoringJobSteps[];
   complete = false;
   originalTrtSaved = false;
 
   private _status: string;
+  private _examId: string;
+  private _assessmentId: string;
+  private _studentName: string;
 
+  get examId(): string {
+    if (this._examId) {
+      return this._examId;
+    }
+
+    return "----"
+  }
+
+  set examId(newExamId: string) {
+    this._examId = newExamId;
+  }
+
+  get assessmentId(): string {
+    if (this._assessmentId) {
+      return this._assessmentId;
+    }
+
+    return "----"
+  }
+
+  set assessmentId(newAssessmentId: string) {
+    this._assessmentId = newAssessmentId;
+  }
+
+  get studentName(): string {
+    if (this._studentName) {
+      return this._studentName;
+    }
+
+    return "----"
+  }
+
+  set studentName(newStudentName: string) {
+    this._studentName = newStudentName;
+  }
+  
   get status(): string {
     if (this._status === 'IN_PROGRESS' && this.isExpired()) {
       return "FAIL - EXPIRED";
