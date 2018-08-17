47032df2fdfb6d3ac5e3384c8384c3bbb7af577c
Ernie Muñoz <emunoz@fairwaytech.com>
2018-08-16 17:13:35 -0700

    Implementing equals/hashcode for ItemScoreParameter

--- client/src/main/java/tds/testpackage/model/ItemScoreParameter.java
+++ client/src/main/java/tds/testpackage/model/ItemScoreParameter.java
@@ -3,8 +3,10 @@ package tds.testpackage.model;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.auto.value.AutoValue;
+import tds.testpackage.legacy.model.Passage;
 
 import javax.xml.bind.annotation.XmlAttribute;
+import java.util.Objects;
 
 @AutoValue
 @JsonDeserialize(builder = AutoValue_ItemScoreParameter.Builder.class)
@@ -25,5 +27,19 @@ public abstract class ItemScoreParameter {
         public abstract Builder setValue(double newValue);
 
         public abstract ItemScoreParameter build();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMeasurementParameter(), getValue());
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final ItemScoreParameter param = (ItemScoreParameter) o;
+        return Objects.equals(getMeasurementParameter(), param.getMeasurementParameter()) &&
+                Objects.equals(getValue(), param.getValue());
     }
 }
