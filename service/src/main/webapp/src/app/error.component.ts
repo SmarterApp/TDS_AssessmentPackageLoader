ee62fbf59d96500e26c3fa69d84ba1e7d9ee611f
Greg Charles <gcharles@fairwaytech.com>
2018-08-31 13:10:36 -0700

    TDS-1636: Provide a link to re-login on error that can appear on timeout

--- service/src/main/webapp/src/app/error.component.ts
+++ service/src/main/webapp/src/app/error.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'error',
-  template: '<h3 class="error">You are not authorized to view the TDS Support Tool application.<br>Please contact an administrator if this is in error.</h3>',
+  template: '<h3 class="error">You are not authorized to view the TDS Support Tool application.<br>Please contact an administrator if this is in error.</h3><a href="javascript:void(0)" onclick="window.document.logoutForm.submit()">Click here to login again</a><form name="logoutForm" method="post" action="saml/logout" class="hidden"></form>',
   styles: ['h3.error { padding: 20px; color: #CC0000; }']
 })
 export class ErrorComponent {
