a749d5f5dc2e30158984ef7886bf27b9fc5b51f3
Greg Charles <gcharles@fairwaytech.com>
2018-08-31 17:51:01 -0700

    TDS-1636: tweaked styles

--- service/src/main/webapp/src/app/error.component.ts
+++ service/src/main/webapp/src/app/error.component.ts
@@ -2,8 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'error',
-  template: '<h3 class="blue">You have been logged out due to inactivity, did not successfully log in,<br>or are not authorized to view this page.</h3><p class="msg">Please <a href="javascript:void(0)" onclick="window.document.logoutForm.submit()">log in again</a> or contact an administrator if this problem persists.</p><form name="logoutForm" method="post" action="saml/logout" class="hidden"></form>',
-  styles: []
+  template: '<p class="blue msg">You have been logged out due to inactivity or are not authorized to view this page.</p><p class="blue msg">Please <a href="javascript:void(0)" onclick="window.document.logoutForm.submit()">log in again</a> or contact an administrator if this problem persists.</p><form name="logoutForm" method="post" action="saml/logout" class="hidden"></form>',
+  styles: ['p.msg { padding: 0 20px}']
 })
 export class ErrorComponent {
 }
