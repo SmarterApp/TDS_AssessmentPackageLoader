b8551568124fbf0d6f279c7a2700fa7dfca0d56b
Greg Charles <gcharles@fairwaytech.com>
2018-08-31 17:16:20 -0700

    TDS-1636: amend message wording and styles

--- service/src/main/webapp/src/app/error.component.ts
+++ service/src/main/webapp/src/app/error.component.ts
@@ -2,8 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: 'error',
-  template: '<h3 class="error">You are not authorized to view the TDS Support Tool application.<br>Please contact an administrator if this is in error.</h3><a href="javascript:void(0)" onclick="window.document.logoutForm.submit()">Click here to login again</a><form name="logoutForm" method="post" action="saml/logout" class="hidden"></form>',
-  styles: ['h3.error { padding: 20px; color: #CC0000; }']
+  template: '<h3 class="blue">You have been logged out due to inactivity, did not successfully log in,<br>or are not authorized to view this page.</h3><p class="msg">Please <a href="javascript:void(0)" onclick="window.document.logoutForm.submit()">log in again</a> or contact an administrator if this problem persists.</p><form name="logoutForm" method="post" action="saml/logout" class="hidden"></form>',
+  styles: []
 })
 export class ErrorComponent {
 }
